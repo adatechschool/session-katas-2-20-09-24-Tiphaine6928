f93dc0b29ffa06cf005f84a4bbcec95a1cd173da
Tiphaine <tiphaine.chapuis69@gmail.com>
2024-09-20 15:41:25 +0200

    Fonction createDeck qui marche et j'ai réussi à exécuter la fonction 52 fois

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // fonction qui "doit" générer 52 cartes
 function createDeck() {
     // déclaration du tableau de cartes puis du tableau de symboles
-    let cartePoker = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let symbolePoker = ["♢", "♠︎", "♣︎", "♡"];
-    for (let i = 0; i < cartePoker.length; i++){
-        let result1 = cartePoker[Math.floor(Math.random() * cartePoker.length)];
-        let result2 = symbolePoker[Math.floor(Math.random() * symbolePoker.length)];
+    let numero = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let symbole = ["♢", "♠︎", "♣︎", "♡"];
+    for (let i = 0; i < numero.length; i++){
+        let result1 = numero[Math.floor(Math.random() * numero.length)];
+        let result2 = symbole[Math.floor(Math.random() * symbole.length)];
         let finalResult = result1 + result2;
         return finalResult;
     }
 }
 
-console.log(createDeck());
+for (let x = 0; x < 52; x++){
+    console.log(createDeck());
+}
